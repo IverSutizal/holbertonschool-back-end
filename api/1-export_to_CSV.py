a047431814a6a9f961f722ce956ff435ea59100f
IverSutizal <iversutizal@gmail.com>
2022-12-04 23:41:06 -0500

    task

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-''' Return information about his/her TODO list progress from an REST API '''
+"""Write a Python script that, using this REST"""
+import csv
 from requests import get
 from sys import argv
 
+#  print('HTTP header: ', response.headers)
+#  print('URL: ', response.url)
+#  print('Status code: ', response.status_code)
 
-def get_api():
-    ''' Gather data from an API '''
-    emp_id = int(argv[1])
-    emp_name = ''
-    tasks_done = 0
-    tasks_total = 0
-    tasks_titles = []
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    id_user = argv[1]
+    users = get('{}users/{}'.format(url, id_user)).json()
+    username = users.get('username')
+    tasks = get('{}todos?userId={}'.format(url, id_user)).json()
+    with open('{}.csv'.format(id_user), 'wt') as file:
+        write_file = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            write_file.writerow([int(id_user), username,
+                                task.get('completed'), task.get('title')])
 
-    users_res = get('https://jsonplaceholder.typicode.com/users').json()
-    for user in users_res:
-        if user['id'] == emp_id:
-            emp_name = user['name']
-            break
-
-    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
-    for task in tasks_res:
-        if task['userId'] == emp_id:
-            if task['completed']:
-                tasks_titles.append(task['title'])
-                tasks_done += 1
-            tasks_total += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
-                                                          tasks_done,
-                                                          tasks_total))
-    for title in tasks_titles:
-        print('\t {}'.format(title))
-
-
-if __name__ == '__main__':
-    get_api()
